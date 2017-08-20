37478f208dc6d5aad4f46ef427b73909781341b5
Karthik Challa <macbookpro@MacBook-Pro-2.local>
2017-08-20 21:15:52 +0530

    Added charts and stackviews, Need to improvise

--- TPL-Sales/Pods/Target Support Files/Pods-TPL-Sales/Pods-TPL-Sales-frameworks.sh
+++ TPL-Sales/Pods/Target Support Files/Pods-TPL-Sales/Pods-TPL-Sales-frameworks.sh
@@ -108,6 +108,7 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/MBProgressHUD/MBProgressHUD.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Realm/Realm.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RealmSwift/RealmSwift.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SideMenuController/SideMenuController.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Charts/Charts.framework"
@@ -116,6 +117,7 @@ if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/MBProgressHUD/MBProgressHUD.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Realm/Realm.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RealmSwift/RealmSwift.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SideMenuController/SideMenuController.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
--- TPL-Sales/TPL-Sales/ViewController.swift
+++ TPL-Sales/TPL-Sales/ViewController.swift
@@ -10,34 +10,74 @@ import UIKit
 import Charts
 
 class ViewController: UIViewController {
-
+    
     // MARK: Outlets
+    
     @IBOutlet weak var pieChart: PieChartView!
     @IBOutlet weak var lineChart: LineChartView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
+        let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+        let unitsSold = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0, 12.0, 2.0, 5.0, 14.0, 2.8, 4.2]
         
-        let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun"]
-        let unitsSold = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0]
+        pieChart.centerText = "Sales"
+        pieChart.chartDescription?.position = CGPoint(x: pieChart.frame.size.width/2, y: 5)
+        pieChart.chartDescription?.textAlign = .justified
+        pieChart.drawEntryLabelsEnabled = false
         
-        setChart(dataPoints: months, values: unitsSold)
+        lineChart.leftAxis.drawGridLinesEnabled = false
+        lineChart.rightAxis.enabled = false
+        lineChart.xAxis.drawGridLinesEnabled = false
+        lineChart.xAxis.labelPosition = .bottom
+        lineChart.xAxis.labelCount = months.count
+        lineChart.xAxis.valueFormatter = IndexAxisValueFormatter(values: months)
+        lineChart.xAxis.granularity = 1.0
+        lineChart.chartDescription?.position = CGPoint(x: lineChart.frame.size.width/2, y: 5)
+        lineChart.chartDescription?.textAlign = .center
         
+        setPieChart(dataPoints: months, values: unitsSold)
+        setLineChart(dataPoints: months, values: unitsSold)
     }
     
-    func setChart(dataPoints: [String], values: [Double]) {
+    override func viewDidAppear(_ animated: Bool) {
+        
+        pieChart.animate(xAxisDuration: 0.0, yAxisDuration: 1.0)
+        lineChart.animate(xAxisDuration: 0.0, yAxisDuration: 1.0)
+    }
+    
+    func setLineChart(dataPoints: [String], values: [Double]) {
         
         var dataEntries: [ChartDataEntry] = []
         
         for i in 0..<dataPoints.count {
-            let dataEntry = ChartDataEntry(x: values[i], y: Double(i))
+            let dataEntry = ChartDataEntry(x: Double(i), y: values[i])
             dataEntries.append(dataEntry)
         }
         
-        let pieChartDataSet = PieChartDataSet(values: dataEntries, label: "Units Sold")
-        let pieChartData = PieChartData(dataSet: pieChartDataSet)
+        let lineChartDataSet = LineChartDataSet(values: dataEntries, label: "Units Sold")
+        let lineChartData = LineChartData(dataSet: lineChartDataSet)
+        lineChart.data = lineChartData
+        
+        lineChart.chartDescription?.text = "Line Chart Demo"
+        lineChart.gridBackgroundColor = .clear
+    }
+    
+    func setPieChart(dataPoints: [String], values: [Double]) {
+        
+        var dataEntries: [PieChartDataEntry] = []
+        
+        for i in 0..<dataPoints.count {
+            let dataEntry = PieChartDataEntry(value: values[i], label: dataPoints[i])
+            dataEntries.append(dataEntry)
+        }
+        
+        let pieChartDataSet = PieChartDataSet(values: dataEntries, label: "")
+
+        let pieChartData = PieChartData()
+        pieChartData.addDataSet(pieChartDataSet)
+        
         pieChart.data = pieChartData
         
         var colors: [UIColor] = []
@@ -50,15 +90,9 @@ class ViewController: UIViewController {
             let color = UIColor(red: CGFloat(red/255), green: CGFloat(green/255), blue: CGFloat(blue/255), alpha: 1)
             colors.append(color)
         }
-        
         pieChartDataSet.colors = colors
         
-        
-        let lineChartDataSet = LineChartDataSet(values: dataEntries, label: "Units Sold")
-        let lineChartData = LineChartData(dataSet: lineChartDataSet)
-        lineChart.data = lineChartData
-        
+        pieChart.chartDescription?.text = "Pie Chart Demo"
     }
-
-
+    
 }
